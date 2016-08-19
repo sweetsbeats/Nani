0ad592802a58ee68402237cf6e25c15b34a44c8d
sweetsbeats <the.patton@yahoo.ca>
2016-08-18 22:39:02 -0700

    NAV BAR STARTO
    moved nani symbol to middle of future navigation bar

--- index.php
+++ index.php
@@ -6,9 +6,11 @@
   <link rel="stylesheet" href="styletest.css"></link>
 </head>
 
-<img class="nonon" src="images/nani.png">
-  <h1>nani?!</h1>
-
+<div class="navbar">
+  <img class="nonon" src="images/nani.png">
+  <p id="leftOfImage">test</p>
+</div>
+<h1>nani?!</h1>
   <div class="streamerbox">
   <img class="streamer" src="images/shinobu.jpg" style="width">
   <p id="streamername">sweetsbeats</p>
